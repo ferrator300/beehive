27aaeb793b1a385c52173c35ce98929e716edb89
Gvila88 <a2106912@institutmontilivi.cat>
2023-02-15 19:46:59 +0100

    Ja carreguen els Logos
    Mitjançant Imports ja hem pogut carregar els logos be

--- src/components/toolbar.vue
+++ src/components/toolbar.vue
@@ -37,6 +37,9 @@
 
 <script>
 import { RouterLink } from 'vue-router';
+import LOGO_Tecnic from '@/assets/LOGO_Tecnic.png';
+import LOGO_Admin from '@/assets/LOGO_Admin.png';
+import LOGO_Gestor from '@/assets/LOGO_Gestor.png';
 
 export default {
     name: 'toolbarComp',
@@ -44,17 +47,23 @@ export default {
     components: {
     RouterLink
 },
+data(){
+  return{
+    img: '@/assets/LOGO_Admin.png' 
+    
+  }
+},
 computed: {
-    avatarPath() {
-      if (this.rol == 'Tecnic') {
-        return 'src/assets/LOGO_Tecnic.png';
-      } else if (this.rol == 'Gestor') {
-        return 'src/assets/LOGO_Admin.png';
+  avatarPath() {
+      if (this.rol === 'Tecnic') {
+        return LOGO_Tecnic;
+      } else if (this.rol === 'Gestor') {
+        return LOGO_Admin;
+      } else {
+        return LOGO_Gestor;
       }
-      else
-        return 'src/assets/LOGO_Gestor.png';
-}
-    },
+    }
+  },
     methods: {
         /* 
             Function: checkRol
